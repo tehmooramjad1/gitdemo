42be1c45afe29ac92f3f6c58707a76be6b84991b
Tehmoor Amjad <tehmoor.amjad@phaedrasolutions.com>
2021-08-25 16:22:29 +0500

    Updated MainActivity

---
+++ app/src/main/java/com/dev175/gitdemo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.dev175.gitdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("TAG", "onCreate: ")
+
+    }
+}
